d77a1af418d6992316da138db53071882b32559e
Hunter Bacot <hsbacot@gmail.com>
2013-01-25 10:57:28 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,40 +19,6 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hun1233209452740');
-
-/** MySQL database username */
-define('DB_USER', 'hun1233209452740');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'f3M1CNVzF');
-
-/** MySQL hostname */
-define('DB_HOST', 'hun1233209452740.db.9802280.hostedresource.com');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'NLynyV6R2FU3tJ5UWYVM');
-define('SECURE_AUTH_KEY',  'avamE@ _nTt_DnBPn+Av');
-define('LOGGED_IN_KEY',    'FSJsC7GcWw-MdgwrWLEc');
-define('NONCE_KEY',        'w0E*-$(GdJ1x*NrFdf3C');
-define('AUTH_SALT',        'W_y=yNpw VKTMHXSPr6k');
-define('SECURE_AUTH_SALT', 'M7r2%wdjJ(P/!-R$7)9I');
-define('LOGGED_IN_SALT',   'D*XK$bKM27VnHv9H4IRt');
-define('NONCE_SALT',       'tURa@-DDCY!7kn5hGRhb');
 
 /**#@-*/
 
